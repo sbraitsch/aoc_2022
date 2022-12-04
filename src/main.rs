723118930df0540d35a611fe162b8c0a1ec34bca
Simon Braitsch <S.Braitsch@gmx.de>
2022-12-04 15:02:15 +0100

    day 4

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@
 #[path = "day_1/solution.rs"] mod day1;
 #[path = "day_2/solution.rs"] mod day2;
 #[path = "day_3/solution.rs"] mod day3;
+#[path = "day_4/solution.rs"] mod day4;
 mod utils;
 
 fn main() {
     //day1::day1();
     //day2::day2();
-    day3::day3();
+    //day3::day3();
+    day4::day4();
 }
