0e16217128ea5c536026a40ed05c9bf5c5fc9506
Simon Braitsch <simon.braitsch@novatec-gmbh.de>
2022-12-05 11:54:40 +0100

    day 5

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,14 @@
 #[path = "day_2/solution.rs"] mod day2;
 #[path = "day_3/solution.rs"] mod day3;
 #[path = "day_4/solution.rs"] mod day4;
+#[path = "day_5/solution.rs"] mod day5;
+
 mod utils;
 
 fn main() {
     //day1::day1();
     //day2::day2();
     //day3::day3();
-    day4::day4();
+    //day4::day4();
+    day5::day5();
 }
