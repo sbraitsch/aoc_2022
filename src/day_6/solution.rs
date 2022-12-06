942024afdf3df0ec62f2c86e32822db27b022faf
Simon Braitsch <simon.braitsch@novatec-gmbh.de>
2022-12-06 13:24:43 +0100

    day 6 refactor p2

--- src/day_6/solution.rs
+++ src/day_6/solution.rs
@@ -4,10 +4,8 @@ use super::utils::read_string;
 
 pub fn day6() {
     let sequence = read_string("src/day_6/input.txt").chars().collect::<Vec<char>>();
-
     let sol_1 = solution_1(&sequence);
     let sol_2 = solution_2(&sequence);
-
     println!("Day 6 | Part 1: {:?}\nDay 6 | Part 2: {:?}", sol_1, sol_2)
 }
 
@@ -20,11 +18,10 @@ fn solution_2(sequence: &Vec<char>) -> usize {
 }
 
 fn index_of_unique_window(sequence: &Vec<char>, window_size: usize) -> usize {
-    for (i, window) in sequence.windows(window_size).enumerate() {
-        let uniques: HashSet<&char> = HashSet::from_iter(window.iter());
-        if uniques.len() == window_size {
-            return i + window_size
-        }
-    }
-    0
+    let marker = sequence
+        .windows(window_size)
+        .enumerate()
+        .find(|(_, w)| HashSet::<_>::from_iter(w.iter()).len() == window_size)
+        .unwrap();
+    marker.0 + window_size
 }
