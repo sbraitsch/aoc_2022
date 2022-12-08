9391e35c2af6111bd08052989327829b7863e65a
Simon Braitsch <S.Braitsch@gmx.de>
2022-12-08 18:04:49 +0100

    day 8 refactor

--- src/day_8/solution.rs
+++ src/day_8/solution.rs
@@ -1,94 +1,79 @@
-use std::{time::Instant, collections::HashMap};
+use std::time::Instant;
 use crate::utils::input_lines;
 
-#[derive(Clone, Debug)]
-struct Tree {
-    height: usize,
-    visible: bool,
-    scenic_score: usize
-}
-
-type Forest = HashMap<(usize, usize), Tree>;
+type Forest = Vec<Vec<usize>>;
 
 pub fn day8() {
     let now = Instant::now();
-    let mut forest: Forest = HashMap::new();
+    let mut forest: Forest = Vec::with_capacity(99);
 
-    for (y, row) in input_lines(8).iter().enumerate() {
-        for (x, c) in row.chars().enumerate() {
-            forest.insert((x, y), Tree { height:c.to_digit(10).unwrap() as usize, visible: true, scenic_score: 1});
-        }
+    for row in input_lines(8).iter() {
+        forest.push(row.chars().map(|c| c.to_digit(10).unwrap() as usize).collect());
     }
 
-    map_visibility(&mut forest);
+    let (part_1, part_2) = map_vis(&forest);
 
-    println!("Day 8 | Part 1: {:?}\nDay 8 | Part 2: {:?}", solution_1(&forest), solution_2(&forest));
+    println!("Day 8 | Part 1: {:?}\nDay 8 | Part 2: {:?}", part_1, part_2);
     println!("Took: {:?}", now.elapsed());
 }
 
-fn solution_1(forest: &Forest) -> usize {
-    forest.iter().fold(0, |acc, (_, tree)| acc + tree.visible as usize)
-}
+fn map_vis(forest: &Forest) -> (usize, usize) {
+    let mut visibles = 0;
+    let mut most_scenic = 0;
 
-fn solution_2(forest: &Forest) -> usize {
-    forest.iter().map(|(_, tree)| tree.scenic_score).max().unwrap()
-}
-
-fn map_visibility(forest: &mut Forest) {
-    let f = forest.clone();
-
-    for ((x, y), tree) in forest.iter_mut() {
-        if !(x == &0 || y == &0 || x == &98 || y == &98) {
-            let mut blocked = 0;
-            let mut cur_vd = 0;
-            let mut scenic_score = 1;
-            
-            //west
-            for (vd, i) in (0..*x).rev().enumerate() {
-                cur_vd = vd+1;
-                if f[&(i, *y)].height >= tree.height {
-                    blocked += 1;
-                    break;
+    for y in 0..forest.len() {
+        for x in 0..forest.len() {
+            if !(x == 0 || y == 0 || x == 98 || y == 98) {
+                let cur_height = forest[y][x];
+                let mut blocked = 0;
+                let mut view_distance = 0;
+                let mut scenic_score = 1;
+                
+                //west
+                for (vd, i) in (0..x).rev().enumerate() {
+                    view_distance = vd+1;
+                    if forest[y][i] >= cur_height {
+                        blocked += 1;
+                        break;
+                    }
                 }
-            }
-            scenic_score *= cur_vd;
-
-            //east
-            for (vd, i) in (*x+1..99).enumerate() {
-                cur_vd = vd+1;
-                if f[&(i, *y)].height >= tree.height {
-                    blocked += 1;
-                    break;
+                scenic_score *= view_distance;
+    
+                //east
+                for (vd, i) in (x+1..99).enumerate() {
+                    view_distance = vd+1;
+                    if forest[y][i] >= cur_height {
+                        blocked += 1;
+                        break;
+                    }
                 }
-            }
-            scenic_score *= cur_vd;
-
-            //north
-            for (vd, i) in (0..*y).rev().enumerate() {
-                cur_vd = vd+1;
-                if f[&(*x, i)].height >= tree.height {
-                    blocked += 1;
-                    break;
+                scenic_score *= view_distance;
+    
+                //north
+                for (vd, i) in (0..y).rev().enumerate() {
+                    view_distance = vd+1;
+                    if forest[i][x] >= cur_height {
+                        blocked += 1;
+                        break;
+                    }
                 }
-            }
-            scenic_score *= cur_vd;
-
-            //south
-            for (vd, i) in (*y+1..99).enumerate() {
-                cur_vd = vd+1;
-                if f[&(*x, i)].height >= tree.height {
-                    blocked += 1;
-                    break;
+                scenic_score *= view_distance;
+    
+                //south
+                for (vd, i) in (y+1..99).enumerate() {
+                    view_distance = vd+1;
+                    if forest[i][x] >= cur_height {
+                        blocked += 1;
+                        break;
+                    }
                 }
-            }
-            scenic_score *= cur_vd;
+                scenic_score *= view_distance;
+                
+                if blocked < 4 { visibles += 1; }
+                if scenic_score > most_scenic { most_scenic = scenic_score; }
 
-            update_map(scenic_score, blocked, tree);
+            } else { visibles += 1 }
         }
     }
-}
-
-fn update_map(scenic_score: usize, blocked: u8, tree: &mut Tree) {
-    if blocked == 4 { tree.visible = false};
-    tree.scenic_score *= scenic_score;
+    (visibles, most_scenic)
 }
