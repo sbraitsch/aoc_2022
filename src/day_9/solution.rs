647b929ec74a24370e5af7e5f945e179d896b91e
Simon Braitsch <S.Braitsch@gmx.de>
2022-12-09 15:55:31 +0100

    day 9 refactor

--- src/day_9/solution.rs
+++ src/day_9/solution.rs
@@ -12,58 +12,41 @@ impl Knot {
         self.x.abs_diff(trailing.x) > 1 || self.y.abs_diff(trailing.y) > 1
     }
 
-    fn adjust_following(self: &Knot, next_knot: &mut Knot) {
-        if self.should_move(next_knot) {
-            let mut x_offset = 0;
-            if self.x > next_knot.x {
-                x_offset = 1;
-            } else if self.x < next_knot.x {
-                x_offset = -1;
-            }
-            let mut y_offset = 0;
-            if self.y > next_knot.y {
-                y_offset = 1;
-            } else if self.y < next_knot.y {
-                y_offset = -1;
-            }
-            next_knot.x += x_offset;
-            next_knot.y += y_offset;
-        }
-    }
-
     fn adjust_tail(self: &Knot, knots: &mut Vec<Knot>) {
         let mut head = self.clone();
         for tail in knots.iter_mut() {
-            head.adjust_following(tail);
-            head = tail.clone();
+            if head.should_move(tail) {
+                tail.x += (head.x - tail.x).clamp(-1, 1);
+                tail.y += (head.y - tail.y).clamp(-1, 1);
+                head = tail.clone();
+            } else { break; }
         }
     }
 }
 
 pub fn day9() {
     let now = Instant::now();
-    println!("Day 9 | Part 1: {:?}\nDay 9 | Part 2: {:?}", solution_1(), solution_2());
+    let input = input_lines(9);
+    println!("Day 9 | Part 1: {:?}\nDay 9 | Part 2: {:?}", solution_1(&input), solution_2(&input));
     println!("Took: {:?}", now.elapsed());
 }
 
-fn solution_1() -> usize {
+fn solution_1(input: &Vec<String>) -> usize {
     let head = Knot { x: 0, y: 0 };
     let tail = vec![Knot { x: 0, y: 0 }];
-
-    move_head(head, tail)
+    move_head(input, head, tail)
 }
 
-fn solution_2() -> usize {
+fn solution_2(input: &Vec<String>) -> usize {
     let head = Knot { x: 0, y: 0 };
     let tail = vec![Knot { x: 0, y: 0 }; 9];
-
-    move_head(head, tail)
+    move_head(input, head, tail)
 }
 
-fn move_head(mut head: Knot, mut tail: Vec<Knot>) -> usize {
-    let mut visited: HashSet<Knot>  = HashSet::new();
+fn move_head(input: &Vec<String>, mut head: Knot, mut tail: Vec<Knot>) -> usize {
+    let mut visited: HashSet<Knot> = HashSet::new();
 
-    for mv in input_lines(9) {
+    for mv in input {
         let distance = &mv[2..].parse::<i32>().unwrap();
         for _ in 0..*distance {
             match mv.chars().nth(0).unwrap() {
@@ -73,13 +56,9 @@ fn move_head(mut head: Knot, mut tail: Vec<Knot>) -> usize {
                 'D' => { head.y += 1; },
                 _ => panic!("Invalid Input")
             }
-            cascade_update(&head, &mut tail, &mut visited);
+            head.adjust_tail(&mut tail);
+            visited.insert(tail.last().unwrap().clone());
         }
     }
     visited.len()
-}
-
-fn cascade_update(head: &Knot, tail: &mut Vec<Knot>, visited: &mut HashSet<Knot>) {
-    head.adjust_tail(tail);
-    visited.insert(tail.last().unwrap().clone());
 }
