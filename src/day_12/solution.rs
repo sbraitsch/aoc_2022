6bd0d20e85a612778520303612eb5e53e230b40f
Simon Braitsch <S.Braitsch@gmx.de>
2022-12-12 17:20:11 +0100

    day 12 refactor

--- src/day_12/solution.rs
+++ src/day_12/solution.rs
@@ -1,14 +1,12 @@
-use std::{time::Instant};
+use std::{time::Instant, collections::BTreeSet};
 
 use crate::utils::input_lines;
 
 #[derive(Debug, Clone)]
 struct Node {
     idx: usize,
-    cost: usize,
-    g_cost: usize,
-    h_cost: usize,
-    prev: Option<usize>
+    elevation: usize,
+    cost: usize
 }
 
 pub fn day12() {
@@ -19,124 +17,84 @@ pub fn day12() {
     let input = input_lines(12);
     let dim_x = input.first().unwrap().len();
     let dim_y = input.len();
-    let mut all_basins: Vec<(usize, usize)> = Vec::new();
 
     for (y, row) in input.iter().enumerate() {
         for (x, height) in row.chars().enumerate() {
             match height {
                 'S' => { 
                     start = (x, y);
-                    map.push(Node { idx: (y * dim_x) + x, cost: 1, g_cost: 0, h_cost: 0, prev: None});
+                    map.push(Node { idx: (y * dim_x) + x, cost: 0, elevation: 1});
                 },
                 'E' => { 
                     target = (x, y);
-                    map.push(Node { idx: (y * dim_x) + x, cost: 26, g_cost: 0, h_cost: 0, prev: None});
+                    map.push(Node { idx: (y * dim_x) + x, cost: 0, elevation: 26});
                 },
                 c => {
                     let height = c as usize - 96;
-                    if height == 1 { all_basins.push((x, y)) }
-                    map.push(Node { idx: (y * dim_x) + x, cost: height, g_cost: 0, h_cost: 0, prev: None});
+                    map.push(Node { idx: (y * dim_x) + x, cost: 0, elevation: height});
                 }
             }
         }
     }
 
-    for node in map.iter_mut() {
-        node.h_cost = calc_h_cost((node.idx % dim_x, node.idx / dim_x), target)
-    }
-
-    println!("Day 12 | Part 1: {:?}\nDay 12 | Part 2: {:?}", solution_1(&mut map.clone(), start, target, dim_x, dim_y), solution_2(&mut map, &mut all_basins, target, dim_x, dim_y));
-    println!("Took: {:?}", now.elapsed());
+    println!("Day 12 | Part 1: {:?}\nDay 12 | Part 2: {:?}", solution_1(&mut map.clone(), start, target, dim_x, dim_y), solution_2(&mut map.clone(), target, dim_x, dim_y));
+    println!("Benchmark: {:?}", now.elapsed());
 }
 
 fn solution_1(map: &mut Vec<Node>, start: (usize, usize), target: (usize, usize), dim_x: usize, dim_y: usize) -> usize {
-    astar(map, start, target, dim_x, dim_y)
+    shortest_path(map, start, target, dim_x, dim_y, false)
 }
 
-fn solution_2(map: &mut Vec<Node>, basins: &mut Vec<(usize, usize)>, target: (usize, usize), dim_x: usize, dim_y: usize) -> usize {
-    let mut current_min = 1000;
-    for basin in basins {
-        let path_len = astar(&mut map.clone(), *basin, target, dim_x, dim_y);
-        if path_len != 0 {
-            println!("Found a path with length <{path_len}> starting from <{:?}>", basin);
-            if path_len < current_min {
-                current_min = path_len;
-            }
-        }
-    }
-    current_min
+fn solution_2(map: &mut Vec<Node>, target: (usize, usize), dim_x: usize, dim_y: usize) -> usize {
+    shortest_path(map, target, (0, 0), dim_x, dim_y, true)
 }
 
 fn get_adjacent<'a>(idx: usize, dim_x: usize, dim_y: usize) -> Vec<usize> {
-    let x = (idx % dim_x) as i16;
-    let y = (idx / dim_x) as i16;
-    let adj = vec![
-        (x, y - 1),
-        (x, y + 1),
-        (x - 1, y),
-        (x + 1, y)
-    ];
+    let x = idx % dim_x;
+    let y = idx / dim_x;
 
-    adj.iter()
-        .filter(|(x, y)| x >= &0 && x <= &((dim_x - 1) as i16) && y >= &0 && y <= &((dim_y - 1) as i16))
-        .map(|(x, y)| get_idx(*x as usize, *y as usize, dim_x))
-        .collect()
-}
+    let mut adj = Vec::new();
+    if x > 0 { adj.push(idx - 1); }
+    if x < dim_x - 1 { adj.push(idx + 1); }
+    if y > 0 { adj.push(idx - dim_x); }
+    if y < dim_y - 1 { adj.push(idx + dim_x); }
 
-fn calc_h_cost(start: (usize, usize), end: (usize, usize)) -> usize {
-    end.0.abs_diff(start.0) + end.1.abs_diff(start.1)
+    adj
 }
 
 fn get_idx(x: usize, y: usize, dim_x: usize) -> usize {
     (y * dim_x) + x
 }
 
-fn min_by_fcost(maze: &Vec<Node>, open: &Vec<usize>) -> (usize, usize) {
-    let mut min_idx = (0, 0);
-    let mut current_min = 1000;
-    for (i, idx) in open.iter().enumerate() {
-        let check = &maze[*idx];
-        let f_cost = check.g_cost + check.h_cost;
-        if f_cost < current_min {
-            current_min = f_cost;
-            min_idx = (i, *idx);
-        }
-    }
-    min_idx
-}
-
-fn astar(maze: &mut Vec<Node>, (start_x, start_y): (usize, usize), (target_x, target_y): (usize, usize), dim_x: usize, dim_y: usize)-> usize {
-    let mut open_list: Vec<usize> = Vec::new();
+fn shortest_path(maze: &mut Vec<Node>, (start_x, start_y): (usize, usize), (target_x, target_y): (usize, usize), dim_x: usize, dim_y: usize, p2: bool)-> usize {
+    let mut open_list = BTreeSet::new();
     let mut closed_list: Vec<usize> = Vec::new();
 
     let target = get_idx(target_x, target_y, dim_x);
-    open_list.push(get_idx(start_x, start_y, dim_x));
+    open_list.insert((0, get_idx(start_x, start_y, dim_x)));
 
     while !open_list.is_empty() {
-        let (open_idx, mut current_idx) = min_by_fcost(&maze, &open_list);
-        open_list.remove(open_idx);
-        if current_idx == target {
-            let mut path_len = 0;
-            while let Some(idx) = maze[current_idx].prev {
-                maze[idx].cost = 0;
-                current_idx = idx;
-                path_len += 1;
-            };
-            return path_len;
+        let (cur_cost, current_idx) = open_list.pop_first().unwrap();
+        let current_node = maze[current_idx].clone();
+        if (p2 && current_node.elevation == 1) || (!p2 && current_idx == target) {
+            return current_node.cost;
         } else {
             closed_list.push(current_idx);
             for adj in get_adjacent(current_idx, dim_x, dim_y) {
+                let adj_node = &mut maze[adj];
                 if !closed_list.contains(&adj) {
-                    if maze[adj].cost as i8 - maze[current_idx].cost as i8 > 1 { continue; };
-                    let cost_through_current = maze[current_idx].g_cost + 1;
-                    if !open_list.contains(&adj) {
-                        maze[adj].g_cost = cost_through_current;
-                        maze[adj].prev = Some(current_idx);
-                        open_list.push(adj);
+                    if adj_node.elevation as i8 - current_node.elevation as i8 > 1 && !p2 { 
+                        continue; 
+                    } else if current_node.elevation as i8 - adj_node.elevation as i8 > 1 && p2 {
+                        continue;
+                    };
+                    let cost_through_current = cur_cost + 1;
+                    if open_list.iter().find(|(_, i)| i == &adj).is_none() {
+                        adj_node.cost = cost_through_current;
+                        open_list.insert((cost_through_current, adj));
                     } else{
-                        if cost_through_current < maze[adj].g_cost {
-                            maze[adj].g_cost = cost_through_current;
-                            maze[adj].prev = Some(current_idx)
+                        if cost_through_current < adj_node.cost {
+                            adj_node.cost = cost_through_current;
                         }
                     }
                 }
